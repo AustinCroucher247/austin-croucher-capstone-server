58ee3ba30b07a83873860ae5c4438b3200c7f833
Austin Croucher <austin.croucher@gmail.com>
2023-04-07 13:35:36 -0400

    changed knex file

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,14 @@ require('dotenv').config();
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
 module.exports = {
     client: 'mysql2',
     connection: {
-        host: '127.0.0.1',
-        database: process.env.DB_LOCAL_DBNAME,
-        user: process.env.DB_LOCAL_USER,
-        password: process.env.DB_LOCAL_PASSWORD,
-    },
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATABASE,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT || 3306 // default port is 3306
+    }
 };
