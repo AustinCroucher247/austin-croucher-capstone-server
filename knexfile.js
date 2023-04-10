a89c4d2c078ad29cd1784764777f6e50cc30d3cc
Austin Croucher <austin.croucher@gmail.com>
2023-04-10 01:08:19 -0400

    trying to get heroku up

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,22 @@ require('dotenv').config();
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+
+const connection = process.env.DATABASE_URL || {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT || 3306,
+};
+
+module.exports = {
+    client: "mysql2",
+    connection: connection,
+};
+
+
 // module.exports = {
 //     client: 'mysql2',
 //     connection: {
@@ -16,13 +32,13 @@ require('dotenv').config();
 
 
 
-module.exports = {
-    client: 'mysql2',
-    connection: {
-        host: process.env.DB_HOST,
-        database: process.env.DB_DATABASE,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT || 3306 // default port is 3306
-    }
-};
+// module.exports = {
+//     client: 'mysql2',
+//     connection: {
+//         host: process.env.DB_HOST,
+//         database: process.env.DB_DATABASE,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//         port: process.env.DB_PORT || 3306 // default port is 3306
+//     }
+// };
